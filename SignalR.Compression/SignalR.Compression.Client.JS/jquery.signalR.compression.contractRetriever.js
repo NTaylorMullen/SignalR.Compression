46a2346674aa5dc1c47be86566f5835797616037
NTaylorMullen <NTaylorMullen@outlook.com>
2013-03-11 01:42:00 -0700

    Added events to the compression addon so that users can access raw (compressed) data if they please.

--- SignalR.Compression/SignalR.Compression.Client.JS/jquery.signalR.compression.contractRetriever.js
+++ SignalR.Compression/SignalR.Compression.Client.JS/jquery.signalR.compression.contractRetriever.js
@@ -5,24 +5,64 @@
     "use strict";
 
     var signalR = $.signalR,
-        savedConnection = signalR.fn.init;
+        savedStart = signalR.prototype.start,
+        events = signalR.events;
 
-    $.signalR.fn.init = function () {
-        var connection = this,
-            compressionData = {
-                decompressResult: [], // Array of Booleans representing if we should decompress an invocation result,
-                contracts: {} // Contracts to abide by when sending/receiving data
-            };
+    signalR.prototype.start = function (options, callback) {
+        var dataType = "json",
+            deferred = $.Deferred(),
+            connection = this,
+            jsonp = false,
+            url = connection.url,
+            startArgs = arguments;
 
-        savedConnection.apply(connection, arguments),
+        if (connection.compression) {
+            if ($.type(options) === "object") {
+                if (options.jsonp === true) {
+                    jsonp = true;
+                }
+                else if (signalR.fn.isCrossDomain(connection.url)) {
+                    jsonp = !$.support.cors;
+                }
+            }
 
-        connection._.compressionData = compressionData;
+            if (jsonp) {
+                dataType = "jsonp";
+            }
 
-        connection.starting(function (result) {
-            compressionData.contracts = result.Contracts;
-        });
+            //
+            if (url.toLowerCase().indexOf("/signalr", url.length - 8) !== -1) {
+                url = url.substr(0, url.length - 8);
+            }
 
-        return connection;
+            url += "/compression/contracts";
+
+            $.ajax({
+                url: url,
+                global: false,
+                cache: false,
+                type: "GET",
+                data: {},
+                dataType: dataType,
+                error: function (error) {
+                    $(connection).triggerHandler(events.onError, [error.responseText]);
+                    deferred.reject("SignalR: Error during contract retrieval request: " + error.responseText);
+                },
+                success: function (result) {
+                    connection.compression._.contracts = result.Contracts;
+
+                    savedStart.apply(connection, startArgs).done(function () {
+                        deferred.resolve(connection);
+                    }).fail(function () {
+                        deferred.reject.apply(this, arguments);
+                    });
+                }
+            });
+
+            return deferred.promise();
+        }
+        
+        return savedStart.apply(connection, startArgs);
     };
 
 }(window.jQuery, window));
