bebefdbd8a0049aba3410af4a124439a171e75c3
NTaylorMullen <NTaylorMullen@outlook.com>
2013-03-17 22:15:04 -0700

    Added CompressionTypeHelperFacts
    - Had to reveal internals from the core server project in order to test it

--- SignalR.Compression/SignalR.Compression.Server/Properties/AssemblyInfo.cs
+++ SignalR.Compression/SignalR.Compression.Server/Properties/AssemblyInfo.cs
@@ -36,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("SignalR.Compression.Server.SystemWeb")]
+[assembly: InternalsVisibleTo("SignalR.Compression.Server.Tests")]
