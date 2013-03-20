bb83b4c7a3e84ef4095e610a47b0853daea9496b
NTaylorMullen <NTaylorMullen@outlook.com>
2013-03-19 23:17:00 -0700

    Fixed the Client.JS test project to accurately boot up a server and run tests against it
    - Pulled over some test utilities from SignalR and the QUnit.Extensions

---
+++ SignalR.Compression/tests/SignalR.Compression.Client.JS.Tests/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+using SignalR.Compression.Server;
+using SignalR.Compression.Server.SystemWeb;
+
+namespace SignalR.Compression.Client.JS.Tests
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalHost.DependencyResolver.Compression().CompressPayloads(RouteTable.Routes);
+            RouteTable.Routes.MapHubs();
+        }
+    }
+}
