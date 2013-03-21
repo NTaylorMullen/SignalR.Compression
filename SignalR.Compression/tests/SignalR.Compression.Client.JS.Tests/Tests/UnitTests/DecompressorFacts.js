a90464e3af34e4bb22f54206809dc5e82280baa1
NTaylorMullen <NTaylorMullen@outlook.com>
2013-03-21 00:02:01 -0700

    Added decompressor facts
    - Did not find any errors with decompression, what the heck?? haha

--- SignalR.Compression/tests/SignalR.Compression.Client.JS.Tests/Tests/UnitTests/DecompressorFacts.js
+++ SignalR.Compression/tests/SignalR.Compression.Client.JS.Tests/Tests/UnitTests/DecompressorFacts.js
@@ -1,46 +1,51 @@
-﻿QUnit.module("Compressor Facts");
+﻿QUnit.module("Decompressor Facts");
 
 (function ($, window) {
     var contracts = [{ "methodInvocationHub": { "EchoPerson": [1, 0] }, "methodReturnerHub": { "GetParent": [2, 0], "GetTeacher": [4, 0] } }, { "methodInvocationHub": { "EchoPerson": [[1, 0]], "EchoStudent": [[3, 0]] } }, { "1": [["Age", [0, 2]], ["FirstName", [0, 0]], ["Guardian", [2, 0]], ["LastName", [0, 0]]], "2": [["Age", [0, 2]], ["Children", [1, 1]], ["FirstName", [0, 0]], ["Guardian", [2, 0]], ["LastName", [0, 0]], ["Mother", [2, 0]]], "3": [["Age", [0, 2]], ["Debt", [0, 2]], ["FirstName", [0, 0]], ["GPA", [0, 2]], ["Guardian", [2, 0]], ["LastName", [0, 0]]], "4": [["Age", [0, 2]], ["FirstName", [0, 0]], ["Guardian", [2, 0]], ["LastName", [0, 0]], ["Students", [3, 1]]] }];
-    var compressor = $.signalR.compression.compressor;
+    var decompressor = $.signalR.compression.decompressor;
     var utilities = $.signalR.compression._.utilities;
 
-    QUnit.test("Compresses complex payloads correctly", function () {
-        var uncompressed = {
-            FirstName: "John",
-            LastName: "Doe",
-            Age: 38
-        },
-        expectedCompressed = [38, "John", 0, "Doe"],
-        contract = utilities.getContract(1,contracts);
-
-        QUnit.equal(window.JSON.stringify(compressor.compress(uncompressed, contract, contracts)), window.JSON.stringify(expectedCompressed), "Can accurately compress a Person payload.");
-
-        uncompressed = {
-            FirstName: "John",
-            LastName: "Doe",
+    QUnit.test("Decompresses complex payloads correctly", function () {
+        var expectedDecompressed = {
             Age: 38,
+            FirstName: "John",
+            Guardian: null,
+            LastName: "Doe"
+        },
+        compressed = [38, "John", 0, "Doe"],
+        contract = utilities.getContract(1, contracts);
+
+        QUnit.equal(window.JSON.stringify(decompressor.decompress(compressed, contract, contracts)), window.JSON.stringify(expectedDecompressed), "Can accurately decompress a Person payload.");
+
+        expectedDecompressed = {
+            Age: 38,
+            FirstName: "John",
             Guardian: {
-                LastName: "Boop",
                 Age: 72,
-                FirstName: "Betty",
                 Children: [
                     {
                         Age: 8,
                         FirstName: "Bippi",
+                        Guardian: null,
                         LastName: "Boop"
                     },
                     {
                         Age: 7,
-                        LastName: "Boop",
-                        FirstName: "Bobby"
+                        FirstName: "Bobby",
+                        Guardian: null,
+                        LastName: "Boop"
                     }
-                ]
-            }
+                ],
+                FirstName: "Betty",
+                Guardian: null,
+                LastName: "Boop",
+                Mother: null
+            },
+            LastName: "Doe"
         };
-        expectedCompressed = [38, "John", 
+        compressed = [38, "John",
             [
-                72, 
+                72,
                 [
                     [8, "Bippi", 0, "Boop"],
                     [7, "Bobby", 0, "Boop"]
@@ -49,6 +54,6 @@
             ], "Doe"];
         contract = utilities.getContract(1, contracts);
 
-        QUnit.equal(window.JSON.stringify(compressor.compress(uncompressed, contract, contracts)), window.JSON.stringify(expectedCompressed), "Can accurately compress a complex Person payload.");
+        QUnit.equal(window.JSON.stringify(decompressor.decompress(compressed, contract, contracts)), window.JSON.stringify(expectedDecompressed), "Can accurately compress a Person payload.");
     });
 })($, window);
